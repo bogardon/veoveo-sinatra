e3fe352a7390fea36c5cc05e1ad921be117d7c9d
John Z Wu <bogardon@gmail.com>
2013-06-03 16:16:34 -0700

    configured app so it uses active record as the orm for postgresql.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'thin'
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "pg"
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 # app.rb
-
+require 'rubygems'
+require 'bundler'
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments'
+require './models/user'
+require './models/facebook'
 
 get '/' do
   'Hello world!'
