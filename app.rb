61a13b4079e14579f8dce1339b78fe1d56a9fb03
John Z Wu <bogardon@gmail.com>
2013-06-04 18:24:11 -0700

    refactored require statements

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 # app.rb
-require 'rubygems'
-require 'bundler'
-Bundler.require
-
-require './config/environments'
-require './models/user'
-require './models/facebook'
 
 before do
   content_type 'application/json'
 end
 
 get '/' do
-  "Hello, world!!"
+  "Hello, world!"
+end
+
+post 'facebook_connect' do
+
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,8 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+require './config/environments'
+require './models/user'
+require './models/facebook'
 require './app'
 run Sinatra::Application
