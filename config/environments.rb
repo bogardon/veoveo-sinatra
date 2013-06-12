98cfabcaffc6c9bb9b4f019a21947f906cb327a0
John Z Wu <bogardon@gmail.com>
2013-06-11 17:09:58 -0700

    [environemnts] configured paperclip here.

--- config/environments.rb
+++ config/environments.rb
@@ -3,4 +3,8 @@ configure :development do
   db_config = YAML.load(ERB.new(File.read("./config/database.yml")).result)
   db = db_config[environment]
   ActiveRecord::Base.establish_connection(db)
+
+  s3_config = YAML.load(File.read("./config/s3.yml"))[environment]
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_credentials] = s3_config
 end
